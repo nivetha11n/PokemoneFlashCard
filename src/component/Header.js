b2f74aa216aab81feb21fa12a3e63639eb60b3a9
Nagarajan <Nivetha.Nagarajan@emc.com>
2022-03-11 15:31:45 +0530

    Pokemon App

---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="p-3">
+        <h1>Pokemon Flash Cards</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
